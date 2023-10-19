0e9643f8cd1455a08cd3f768c39cb4a661ef81e2
AtoLrn <antoine.lorin2@gmail.com>
2023-10-19 20:32:38 +0200

    update remix template

--- remix/src/root.tsx
+++ remix/src/root.tsx
@@ -1,55 +1,60 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  LiveReload,
-  Scripts,
-  useLoaderData,
-  Link,
-  Await
-} from "@remix-run/react";
-import { defer } from "@remix-run/node";
-import { Suspense } from "react";
+	Links,
+	Meta,
+	Outlet,
+	LiveReload,
+	Scripts,
+	useLoaderData,
+	Link
+} from '@remix-run/react'
+import { LinksFunction, json } from '@remix-run/node'
+import { GoogleTagManager, GoogleTagManagerIframe } from './utils/gtm/GoogleTagManager'
+import { getEnvironnement } from './utils/gtm/env/environnement.server'
+import stylesheet from './tailwind.css'
 
+export const links: LinksFunction = () => [
+	{ rel: 'stylesheet', href: stylesheet },
+]
+  
 export async function loader() {
-  const contactsPromise = new Promise<string>((resolve) => {
-    setTimeout(() => {
-      resolve("World")
-    }, 2000)
-  })
+	const contactsPromise = new Promise<string>((resolve) => {
+		setTimeout(() => {
+			resolve('World')
+		}, 2000)
+	})
 
-  return defer({ contacts: contactsPromise });
-};
+	return json({ contacts: await contactsPromise, ...getEnvironnement() })
+}
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+	const { contacts, GOOGLE_TAG_MANAGER_ID, NODE_ENV } = useLoaderData<typeof loader>()
 
-  return (
-    <html>
-      <head>
-        <link
-          rel="icon"
-          href="data:image/x-icon;base64,AA"
-        />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-      
-      <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={contacts}>
-          {(data) => <h1>Hello { data }! </h1>}
-        </Await>
-      </Suspense>
+	return (
+		<html>
+			<head>
+				<link
+					rel="icon"
+					href="data:image/x-icon;base64,AA"
+				/>
+				<Meta />
+				<Links />
+				<GoogleTagManager id={GOOGLE_TAG_MANAGER_ID}/>
+
+			</head>
+			<body suppressHydrationWarning={NODE_ENV === 'development'}>
+				<GoogleTagManagerIframe id={GOOGLE_TAG_MANAGER_ID}/>
+
+				<h1 className='text-3xl font-bold underline'>Hello { contacts }! </h1>
+
         
-        <Link to={`/test`}>
-        <button>Navigate</button>
-        </Link>
-        <Outlet />
+				<Link to={'/test'}>
+					<button>Navigate</button>
+				</Link>
+				<Outlet />
 
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
--- remix/src/routes/hello-world.tsx
+++ remix/src/routes/hello-world.tsx
@@ -1,9 +1,9 @@
-import { useRevalidator } from "@remix-run/react";
+import { useRevalidator } from '@remix-run/react'
 
 export default function Test() {  
-    const revalidator = useRevalidator(); 
+	const revalidator = useRevalidator() 
 
-    return (
-      <button onClick={() => { revalidator.revalidate() }}> { revalidator.state === 'loading' ? 'Reloading Data' : 'Data'}</button>
-    );
-  }
+	return (
+		<button onClick={() => { revalidator.revalidate() }}> { revalidator.state === 'loading' ? 'Reloading Data' : 'Data'}</button>
+	)
+}
