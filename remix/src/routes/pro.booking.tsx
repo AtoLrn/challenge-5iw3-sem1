2cd2daacacbd793a42432f7d42e6c99ef92c9796
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-12-06 16:04:15 +0100

    feat(remix): Improve pro booking page.

--- remix/src/routes/pro.booking.tsx
+++ remix/src/routes/pro.booking.tsx
@@ -1,38 +1,25 @@
 import { MetaFunction } from '@remix-run/react'
 import { Title } from 'src/components/Title'
-import { json } from '@remix-run/node'
 import { t } from 'i18next'
-import { ProfileInterface } from 'src/utils/types/profileForm'
-
-type Clients = {
-	profile: ProfileInterface,
-	prestation: Prestation,
-	date: string,
-	duration: number,
-	status: 'accepted' | 'pending' | 'refused'
-}
-
-type Prestation = {
-	name: string,
-	kind: 'tattoo' | 'jewelry' | 'barber',
-	location: string,
-	picture: string,
-}
+import { FaGoogle, FaMicrosoft, FaCalendarDay, FaDownload } from 'react-icons/fa6'
+import { createGoogleCalendarLink, createOutlookCalendarLink, exportToICS } from 'src/utils/exportCalendar'
+import { Booking } from 'src/utils/types/booking'
+import { Link } from '@remix-run/react'
+import { Validation } from 'src/utils/types/validation'
+import { motion as m } from 'framer-motion'
+import { formatDistanceToNow, parseISO, format, isPast } from 'date-fns'
 
 export const meta: MetaFunction = () => {
 	return [
 		{
-			title: 'Booking'
+			title: t('booking-title'),
+			description: t('booking-page')
 		}
 	]
 }
 
-export const loader = () => {
-	return json({ accessToken: process.env.MAP_BOX_TOKEN })
-}
-
 export default function () {
-	const clients: Clients[] = [
+	const clients: Booking[] = [
 		{
 			profile: {
 				username: 'Sam',
@@ -46,9 +33,9 @@ export default function () {
 				location: '32 rue de Cambrai, 75019 Paris, France',
 				picture: 'https://a.pinatafarm.com/407x407/6087855680/laughing-kid.jpg'
 			},
-			date: '2021-08-12 14:30',
+			date: '2023-12-12 14:30',
 			duration: 60,
-			status: 'accepted'
+			status: Validation.ACCEPTED
 		},
 		{
 			profile: {
@@ -63,63 +50,33 @@ export default function () {
 				location: '32 rue de Cambrai, 75019 Paris, France',
 				picture: 'https://a.pinatafarm.com/407x407/6087855680/laughing-kid.jpg'
 			},
-			date: '2023-12-06 14:30',
+			date: '2023-12-06 17:30',
 			duration: 60,
-			status: 'pending'
+			status: Validation.PENDING
 		}
 	]
 
-	const createGoogleCalendarLink = (client: Clients) => {
-		const title = encodeURIComponent(`${client.prestation.name} pour ${client.profile.username}`);
-		const description = encodeURIComponent(`Rendez-vous avec ${client.profile.username}`);
-		const location = encodeURIComponent(client.prestation.location);
+	const getAppointmentTime = (client: Booking) => {
+		const appointmentDate = parseISO(client.date);
+		const timeUntilAppointment = formatDistanceToNow(appointmentDate, { addSuffix: true });
 
-		const startDate = new Date(client.date).toISOString().replace(/-|:|\.\d\d\d/g,"");
-		const endDate = new Date(new Date(client.date).getTime() + client.duration * 60000).toISOString().replace(/-|:|\.\d\d\d/g, '');
+		if (isPast(appointmentDate)) {
+			return t('appointment-passed');
+		}
 
-		const googleCalendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${title}&dates=${startDate}/${endDate}&details=${description}&location=${location}&sf=true&output=xml`;
+		let message;
+		if (appointmentDate.getTime() - new Date().getTime() < 3600000) {
+			message = `${timeUntilAppointment}`;
+		} else if (appointmentDate.getTime() - new Date().getTime() < 86400000) {
+			message = `${timeUntilAppointment}`;
+		} else if (appointmentDate.getTime() - new Date().getTime() < 604800000) {
+			message = `${timeUntilAppointment}`;
+		} else {
+			message = format(appointmentDate, 'dd/MM/yyyy à HH:mm');
+		}
 
-		return googleCalendarUrl;
-	}
-
-	const createOutlookCalendarLink = (client: Clients) => {
-		const title = encodeURIComponent(`${client.prestation.name} pour ${client.profile.username}`);
-		const description = encodeURIComponent(`Rendez-vous avec ${client.profile.username}`);
-		const location = encodeURIComponent(client.prestation.location);
-
-		const startDate = new Date(client.date).toISOString().replace(/-|:|\.\d\d\d/g,"");
-		const endDate = new Date(new Date(client.date).getTime() + client.duration * 60000).toISOString().replace(/-|:|\.\d\d\d/g, '');
-
-		const outlookCalendarUrl = `https://outlook.live.com/calendar/0/deeplink/compose?subject=${title}&body=${description}&location=${location}&startdt=${startDate}&enddt=${endDate}`;
-
-		return outlookCalendarUrl;
-	}
-
-	const exportToICS = (client: Clients) => {
-		const start = new Date(client.date).toISOString().replace(/-|:|\.\d\d\d/g, '');
-		const end = new Date(new Date(client.date).getTime() + client.duration * 60000).toISOString().replace(/-|:|\.\d\d\d/g, '');
-
-		const icsData = [
-			'BEGIN:VCALENDAR',
-			'VERSION:2.0',
-			'BEGIN:VEVENT',
-			`DTSTART:${start}`,
-			`DTEND:${end}`,
-			`SUMMARY:${client.prestation.name} pour ${client.profile.username}`,
-			`DESCRIPTION:Rendez-vous avec ${client.profile.username}`,
-			`LOCATION:${client.prestation.location}`,
-			'END:VEVENT',
-			'END:VCALENDAR'
-		].join('\n');
-
-		const blob = new Blob([icsData], { type: 'text/calendar;charset=utf-8;' });
-		const link = document.createElement('a');
-		link.href = URL.createObjectURL(blob);
-		link.download = `Rendez-vous-${client.profile.username}.ics`;
-		document.body.appendChild(link);
-		link.click();
-		document.body.removeChild(link);
-	}	
+		return message;
+	};
 
 	const exportToCSV = () => {
 		let csvContent = "data:text/csv;charset=utf-8,"
@@ -144,7 +101,6 @@ export default function () {
 		const encodedUri = encodeURI(csvContent);
 		const link = document.createElement("a");
 		link.setAttribute("href", encodedUri);
-		// Today's date with hours and minutes
 		const today = new Date();
 		link.setAttribute("download", today.toLocaleDateString() + '_appointements.csv');
 		document.body.appendChild(link); // Required for FF
@@ -156,37 +112,47 @@ export default function () {
 	return <div className="flex-1 p-8 flex flex-col gap-8 text-white">
 		<Title kind="h2">{t('your-nexts-appointement')}</Title>
 		<section className='flex flex-col gap-4 items-center bg-zinc-950 p-4 rounded-md'>
-			<span className='font-bold'>{t('booking')}</span>
+			<span className='font-bold'>{t('booking-title')}</span>
 			{clients.map((client, index) => {
-				return <div key={index} className='flex w-full p-2 border-b-1 border-gray-200 border-opacity-30 gap-2'>
-					<img key={index} className={ 'rounded-full relative border-2 border-gray-900 object-cover w-8 h-8' } src={client.profile.avatar} alt="Client avatar" />
-					<div className='flex flex-col'>
-						<span>
-							{client.profile.username}
-						</span>
-						<span>
-							{client.prestation.name}
-						</span>
-						<span>
-							{client.date}
-						</span>
+				return <div key={index} className='flex flex-col md:flex-row w-full p-2 border-b border-gray-200 border-opacity-30 gap-4 items-center'>
+				<img className='rounded-full border-2 border-gray-900 object-cover w-12 h-12 mx-auto md:mx-0' src={client.profile.avatar} alt={t('client-avatar-alt')} />
+
+				<div className='flex-grow text-center md:text-left'>
+					<div className='text-lg font-bold'>{client.profile.username}</div>
+					<div>{client.prestation.name} - <Link to={'https://www.google.com/maps/search/?api=1&query=' + client.prestation.location} target="_blank" rel="noopener noreferrer" className='underline'>{client.prestation.location}</Link></div>
+					<div>{getAppointmentTime(client)} ({format(parseISO(client.date), 'dd/MM/yyyy à HH:mm')})</div>
+					<div>{client.duration} {t('minutes')}</div>
+					<div className={`text-sm ${client.status === Validation.ACCEPTED ? ' text-green-500' : client.status === Validation.PENDING ? ' text-yellow-500' : ' text-red-500'}`}>
+						{client.status === Validation.ACCEPTED ? t('accepted') : client.status === Validation.PENDING ? t('pending') : t('rejected')}
 					</div>
-					<div>
-						<a className='px-4 py-2 bg-gray-700 rounded-lg text-white' href={createGoogleCalendarLink(client)}  target="_blank" rel="noopener noreferrer">
-							{t('export-appointement-google-calendar')}
-						</a>
-						<a href={createOutlookCalendarLink(client)} className='px-4 py-2 bg-gray-700 rounded-lg text-white' target="_blank" rel="noopener noreferrer">
-							{t('export-appointement-outlook-calendar')}
-						</a>
-						<button className='px-4 py-2 bg-gray-700 rounded-lg text-white' onClick={() => exportToICS(client)}>
-							{t('export-appointement-ics')}
-						</button>
-					</div>
-				</div> 
+				</div>
+
+				<div className='flex gap-2 justify-center md:justify-start'>
+					<a className='p-2 bg-gray-700 hover:bg-gray-800 rounded-lg text-white' href={createGoogleCalendarLink(client)} target="_blank" rel="noopener noreferrer" title={t('add-google-calendar')}>
+						<FaGoogle />
+					</a>
+					<a className='p-2 bg-gray-700 hover:bg-gray-800 rounded-lg text-white' href={createOutlookCalendarLink(client)} target="_blank" rel="noopener noreferrer" title={t('add-outlook-calendar')}>
+						<FaMicrosoft />
+					</a>
+					<button className='p-2 bg-gray-700 hover:bg-gray-800 rounded-lg text-white' onClick={() => exportToICS(client)} title={t('download-as-ics')}>
+						<FaCalendarDay />
+					</button>
+				</div>
+
+				<Link to={`/booking/${client.profile.username}`} className='p-2 bg-blue-500 hover:bg-blue-600 rounded-lg text-white'>
+					{t('see')}
+				</Link>
+			</div>			
 			})}
 		</section>
-		<button className='px-4 py-2 bg-gray-700 rounded-lg text-white' onClick={exportToCSV}>
-			{t('export-appointements-csv')}
-		</button>
+		<m.button
+			whileHover={{ scale: 1.025 }}
+			className='flex items-center justify-center px-4 py-2 bg-gray-700 rounded-lg text-white'
+			onClick={exportToCSV}
+		>
+			{t('export-appointements-csv')} 
+			<FaDownload className="ml-2" />
+		</m.button>
+
 	</div>
 }
