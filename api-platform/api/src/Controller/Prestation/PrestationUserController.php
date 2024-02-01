0c1297e2e12e5771c2810e49c369d0ac81457a7f
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-02-01 20:11:24 +0100

    feat(api-platform): Fix backend feedback.

--- api-platform/api/src/Controller/Prestation/PrestationUserController.php
+++ api-platform/api/src/Controller/Prestation/PrestationUserController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Prestation;
 use App\Repository\PrestationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Response;
 
 class PrestationUserController extends AbstractController
 {
@@ -15,20 +14,10 @@ class PrestationUserController extends AbstractController
     ) {
     }
 
-    public function __invoke(): Response
+    public function __invoke(): array
     {
         $user = $this->security->getUser();
-        if (!$user) {
-            return $this->json([], Response::HTTP_FORBIDDEN);
-        }
 
-        // Verify if the user is an admin
-        if ($this->security->isGranted('ROLE_ADMIN')) {
-            $prestations = $this->prestationRepository->findAll();
-        } else {
-            $prestations = $this->prestationRepository->findBy(['proposedBy' => $user]);
-        }
-
-        return $this->json($prestations);
+        return $this->prestationRepository->findBy(['proposedBy' => $user]);
     }
 }
