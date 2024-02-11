e81af3f0b3890fa15279cde0b1b49d65475aff2e
Nicolas Barbarisi <nico.bar2012@gmail.com>
2024-02-11 21:34:57 +0100

    feat(api-platform) : Add HasLifecycleCallbacks on feedback entity.

--- api-platform/api/src/Entity/Feedback.php
+++ api-platform/api/src/Entity/Feedback.php
@@ -16,6 +16,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FeedbackRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 #[ApiResource(
     operations: [
         new GetCollection(
