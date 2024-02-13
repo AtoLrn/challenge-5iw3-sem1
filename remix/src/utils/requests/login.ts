4ce79a3ea58ca4542b71a626a574303791bc2f6e
AtoLrn <antoine.lorin2@gmail.com>
2024-02-13 21:16:09 +0100

    debug: check error in prod 2

--- remix/src/utils/requests/login.ts
+++ remix/src/utils/requests/login.ts
@@ -7,25 +7,31 @@ const schema = z.object({
 
 
 export const login = async (props: Login): Promise<string> => {
-	const res = await fetch(`${process.env.API_URL}/login`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(props)
-	})
-
-	const body = await res.json()
-
-	console.log('ANTOINE: ', body)
-
-	const { token, message } = schema.parse(body)
-
-	if (message || !token) {
-		throw new Error(message ?? 'Error in the request')
+	try {
+		const res = await fetch(`${process.env.API_URL}/login`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(props)
+		})
+	
+		const body = await res.json()
+	
+		console.log('ANTOINE: ', body)
+	
+		const { token, message } = schema.parse(body)
+	
+		if (message || !token) {
+			throw new Error(message ?? 'Error in the request')
+		}
+		
+		return token
+	} catch (e) {
+		console.log('ANTOINE2: ', e)
+		throw new Error('Error in the request')
 	}
-    
-	return token
+	
 }
 
 export interface Login {
